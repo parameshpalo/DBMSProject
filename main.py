d7250086748f6e06c427dcf5958de9ade834666c
paramesh palo <paramesh.palo@drona.app>
2025-06-11 23:49:54 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from routers import instruments
+from routers import instruments,booking,users
 
 app=FastAPI()
 
 app.include_router(instruments.router)
+app.include_router(booking.router)
+app.include_router(users.router)
 
 @app.get('/')
 def root():
